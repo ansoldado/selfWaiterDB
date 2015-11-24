378adf635d04efee23ec39ef4f773e9c4a32c088
ansoldado <ansoldado@gmail.com>
2015-11-24 18:03:13 +0100

    socket.io

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var app = express();
+var io = require("socket.io").listen(3000);
+
+io.socket.on("connection", function(socket){
+   socket.on("message", function(){
+      console.log("mensaje recibido");
+   });
+    console.log("socket conectado");
+});
 
 mongoose.connect("mongodb://user:user@ds037814.mongolab.com:37814/heroku_v3hmj82h", function(err, res){
     if(err){
