414f4f1c219bebb23fc3ac2a4ed7c1310a2244b6
ansoldado <ansoldado@gmail.com>
2015-12-01 16:45:48 +0100

    asdf

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ require("./routes")(app);
 var io = require("socket.io").listen(app.listen(process.env.PORT || 5000));
 
 io.sockets.on('connection', function (socket) {
-    function registrarOtro(data){
+    function registrarOtro(data, socket){
         console.info("ESCUCHANDO EN waiter"+data);
 
-        this.socket.on('waiter'+data, function(data2){
+        socket.on('waiter'+data, function(data2){
             console.info("ESTA ESCUCHANDO THIS ");
             if(data2.mode == 'modificacion') {
                 io.sockets.emit('reciveFromServer'+data2.mesa, 'ticket');
@@ -45,7 +45,7 @@ io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('register', function (data) {
         console.info("Registrando mesa : "+data);
-        registrarOtro(data);
+        registrarOtro(data, socket);
         socket.on('table'+data, function(data2){
             console.info("enviando por el socket: "+'table'+data+'camarero');
             io.sockets.emit('table'+data+'camarero', data2);
